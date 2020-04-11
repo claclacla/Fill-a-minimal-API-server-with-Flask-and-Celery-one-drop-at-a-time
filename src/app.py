44f23ae5dcb0f1b73350c3fd891df56d6d9b2f1f
Adelchino Simone <seconddesire@gmail.com>
2020-04-11 22:48:10 +0200

    Added MongoDB

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://mongodb:27017/appDB"
+mongo = PyMongo(app)
 
 @app.route('/course', methods=["POST"])
 def insert_course():
-    return 'Hello, World!'
+    courseRequestDTO = request.json
+    mongo.db.courses.insert_one(courseRequestDTO)
+    return courseRequestDTO["title"]
